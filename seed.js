52f68ef0d9dd5a9a8f2c718ee166c7fbc2beeb7e
simei-yeh <simei.yeh@gmail.com>
2020-11-14 16:19:40 -0800

    removed s3 URL from git commits

--- seed.js
+++ seed.js
@@ -1,7 +1,7 @@
 const db = require('./db/index.js')
 const faker = require('faker')
+const seedURL = require('./seed_URL.js')
 
-const s3BaseURL = 'https://s3-us-west-1.amazonaws.com/trulia.tour.scheduler/'
 const availTimes = ['9:00 AM', '9:30 AM', '10:00 AM', '10:30 AM', '11:00 AM', '11:30 AM', '12:00 PM', '12:30 PM', '1:00 PM', '1:30 PM',
 '2:00 PM', '2:30 PM', '3:00 PM', '3:30 PM', '4:00 PM', '4:30 PM', '5:00 PM', '5:30 PM', '6:00 PM', '6:30 PM', '7:00 PM', '7:30 PM', '8:00 PM', '8:30 PM','9:00 PM']
 
@@ -40,7 +40,7 @@ var createAgentInfo = function () {
     reviewsScore: (Math.random() * 5).toFixed(1),
     reviewsCount: Math.floor(Math.random() * 20),
     recentSales: Math.floor(Math.random() * 20),
-    Picture: `${s3BaseURL}${agentPicture}.jpg`,
+    Picture: `${seedURL}${agentPicture}.jpg`,
     }
   return agent;
 }
