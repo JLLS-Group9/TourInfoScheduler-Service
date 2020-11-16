c6b49e2679f35a56b22267278c5031b4fe7eb38e
simei-yeh <simei.yeh@gmail.com>
2020-11-15 23:01:38 -0800

    Updated for airbnb ES Lint styling

--- client/agentsList.jsx
+++ client/agentsList.jsx
@@ -1,15 +1,14 @@
-import React from 'react'
-
+import React from 'react';
 
 const Agents = ({ agents }) => (
   <span>
-    {agents.map((agent, index) =>
-    <span key={index}>
-      <label>{index ? 'Premier Agent' : 'Listing Agent'}</label>
-      <img  src={agent.picture}></img>
-
-    </span> )}
+    {agents.map((agent, index) => (
+      <span key={index}>
+        <label>{index ? 'Premier Agent' : 'Listing Agent'}</label>
+        <img src={agent.picture} />
+      </span>
+    ))}
   </span>
-)
+);
 
 export default Agents;
