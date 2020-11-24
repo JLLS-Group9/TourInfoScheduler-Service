f793873bb302cbb7cdad2b33adec0a5fdd43c3fb
simei-yeh <simei.yeh@gmail.com>
2020-11-23 16:39:32 -0800

    Updated styles

--- client/form.jsx
+++ client/form.jsx
@@ -181,7 +181,8 @@ class Form extends React.Component {
     rows = rows.concat([
       <div className={styles.finBox}>
         <input type="checkbox" id="checkbox" key="checkbox" className={styles.finCheckBox} onClick={this.toggleFinancing} />
-        <label key="fin" htmlFor="checkbox" className={styles.finLabel}>{finLabel}</label>
+        <label key="fin" htmlFor="checkbox" className={styles.finLabel}>
+          {finLabel}</label>
       </div>,
       <div><button type="submit" className={styles.bigButton}>{isScheduleOn ? 'Schedule a Tour' : 'Request Info'}</button></div>,
     ]);
