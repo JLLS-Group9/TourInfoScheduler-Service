e7e1bf12a95cb5e32ecea5cc9caa993c043ee4e3
simei-yeh <simei.yeh@gmail.com>
2020-11-23 16:25:33 -0800

    Updated layout styling

--- server/index.js
+++ server/index.js
@@ -1,19 +1,19 @@
 const express = require('express');
-
+const morgan = require('morgan');
 const app = express();
 const port = 8081;
 const controller = require('../controllers/index.js');
 
 app.use(express.json());
+app.use(morgan('dev'));
 app.use('/', express.static('public'));
-app.use('/api/homes/:id/', express.static('public'));
+app.use('*/tour/:id/', express.static('public'));
 
+app.use('*/:id/scheduleTour', controller.scheduleTour);
 
-app.use('/api/homes/:id/scheduleTour', controller.scheduleTour);
+app.use('*/:id/requestInfo', controller.requestInfo);
 
-app.use('/api/homes/:id/requestInfo', controller.requestInfo);
-
-app.use('/api/homes/:id/bookings', controller.retrieveBookings);
+app.use('*/:id/bookings', controller.retrieveBookings);
 
 app.listen(port, () => {
   console.log(`Example app listening at http://localhost:${port}`);
