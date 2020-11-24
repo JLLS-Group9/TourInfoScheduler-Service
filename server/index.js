7041a00dd2746e7e2636a08118d53f9a76f7995e
simei-yeh <simei.yeh@gmail.com>
2020-11-23 17:31:13 -0800

    Updated server index.js

--- server/index.js
+++ server/index.js
@@ -7,13 +7,13 @@ const controller = require('../controllers/index.js');
 app.use(express.json());
 app.use(morgan('dev'));
 app.use('/', express.static('public'));
-app.use('*/tour/:id/', express.static('public'));
+app.use('/api/homes/:id/', express.static('public'));
 
-app.use('*/:id/scheduleTour', controller.scheduleTour);
+app.use('/api/homes/:id/scheduleTour', controller.scheduleTour);
 
-app.use('*/:id/requestInfo', controller.requestInfo);
+app.use('/api/homes/:id/requestInfo', controller.requestInfo);
 
-app.use('*/:id/bookings', controller.retrieveBookings);
+app.use('/api/homes/:id/bookings', controller.retrieveBookings);
 
 app.listen(port, () => {
   console.log(`Example app listening at http://localhost:${port}`);
