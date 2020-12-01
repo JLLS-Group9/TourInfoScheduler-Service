fc46c1a1aeeac86fdc1413c70f45ef96a715a27c
simei-yeh <simei.yeh@gmail.com>
2020-11-30 17:23:36 -0800

    Updated compression for server side

--- server/index.js
+++ server/index.js
@@ -3,8 +3,21 @@ const morgan = require('morgan');
 const app = express();
 const port = 8081;
 const controller = require('../controllers/index.js');
+const compression = require('compression')
 
 app.use(express.json());
+app.use(compression({ filter: shouldCompress }))
+
+function shouldCompress (req, res) {
+  if (req.headers['x-no-compression']) {
+    // don't compress responses with this request header
+    return false
+  }
+
+  // fallback to standard filter function
+  return compression.filter(req, res)
+}
+
 app.use(morgan('dev'));
 app.use('/', express.static('public'));
 app.use('/api/homes/:id/', express.static('public'));
